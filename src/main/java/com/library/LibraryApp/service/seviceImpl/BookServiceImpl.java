c386e17d2cc657cb38a99586a7ee0303c053d274
Laide <endeleylaide@gmail.com>
2021-03-30 20:19:29 +0100

    A Simple Rest API for a small library.
    Fixed the update bug on BookServiceImpl

---
+++ src/main/java/com/library/LibraryApp/service/seviceImpl/BookServiceImpl.java
@@ -0,0 +1,58 @@
+package com.library.LibraryApp.service.seviceImpl;
+
+import com.library.LibraryApp.dtos.BookRequest;
+import com.library.LibraryApp.entities.Book;
+import com.library.LibraryApp.service.BookService;
+import org.springframework.stereotype.Service;
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    private Set<Book> books = new HashSet<>();
+
+    @Override
+    public Set<Book> addBook(BookRequest book, int bookId) {
+        books.add(new Book(bookId, book.getTitle()));
+        return books;
+    }
+
+    public Book getBookById(int bookId) {
+        Book bookToGet = null;
+        for(Book singleBook : books){
+            if(singleBook.getId() == bookId){
+                bookToGet = new Book(bookId, singleBook.getTitle());
+                break;
+            }
+        }
+        return bookToGet;
+    }
+
+    public Set<Book> getAllBooks() {
+        return books;
+    }
+
+    public Book updateBookById(BookRequest book, int bookId) {
+        Book bookUpdated = null;
+        for (Book singleBook : books) {
+            if(singleBook.getId() == bookId){
+                books.remove(singleBook);
+                bookUpdated = new Book(bookId, book.getTitle());
+                books.add(bookUpdated);
+                break;
+            }
+        }
+        return bookUpdated;
+    }
+
+    public Set<Book> deleteBookById(int bookId) {
+        for (Book singleBook: books) {
+            if(singleBook.getId() == bookId){
+                books.remove(singleBook);
+                break;
+            }
+        }
+        return books;
+    }
+}
